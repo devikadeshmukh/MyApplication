5932fd21ffa9105c705df5ef95ee5758f5e3736a
Radhika <radhika@radhika.(none)>
2013-08-05 12:43:03 +0530

    hello

---
+++ src/main/java/com/radhikagroup/MyApplication/App.java
@@ -0,0 +1,13 @@
+package com.radhikagroup.MyApplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
